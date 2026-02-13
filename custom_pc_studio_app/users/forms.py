e6fa2b1196d813f9ff9e542b0fb69633df4701b4
Agent_Or <yaroslav.daviduk1@gmail.com>
2026-02-14 00:07:21 +0300

    Profile form controller and notifications are ready

--- custom_pc_studio_app/users/forms.py
+++ custom_pc_studio_app/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -8,11 +8,12 @@ class UserLoginForm(AuthenticationForm):
     username = forms.CharField()
     password = forms.CharField()
 
-    # username = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'autofocus': True, 'class': 'form-control', 'placeholder': 'Введите свое имя'}))
-    # password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class': 'form-control', 'placeholder': 'Введите пароль'}))
     class Meta:
         model = User
-        fields = ['username', 'password'] 
+        fields = {
+            'username', 
+            'password',
+        } 
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -35,3 +36,20 @@ class UserRegistrationForm(UserCreationForm):
             'password2',
         }
 
+class ProfileForm(UserChangeForm):
+    
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    delivery_address = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = {
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'delivery_address',
+        }
--- custom_pc_studio_app/users/models.py
+++ custom_pc_studio_app/users/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 
+    delivery_address = models.TextField(max_length=300, blank=True, null=True, verbose_name="Адрес доставки")
+
     class Meta:
         db_table = 'user'
         verbose_name = 'Пользователя'
